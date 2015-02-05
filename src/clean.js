6492462e2ae58e0e5919ca361300aeff244ae09d
David Bazile <dbazile@bericotechnologies.com>
2015-02-05 12:57:04 -0500

    Code cleanup

--- src/clean.js
+++ src/clean.js
@@ -2,7 +2,27 @@ var fs = require('fs');
 var glob = require('glob');
 var path = require('path');
 
-module.exports = function main(directory, pattern, logger) {
+/**
+ * Try to keep the user from blasting off their foot.
+ *
+ * @param  {string}  pattern Glob pattern
+ * @return {Boolean}
+ */
+function isUnsafePattern(pattern) {
+  var startsWithDotDot = /^\.\./;
+  var startsWithSlash = /^\//;
+
+  return startsWithSlash.test(pattern) || startsWithDotDot.test(pattern);
+}
+
+/**
+ * Main operation.
+ *
+ * @param  {string} directory The root directory to search from
+ * @param  {string} pattern   A glob pattern to use relative to the specified directory
+ * @param  {object} logger    An instance of the mimosa logger object
+ */
+function main(directory, pattern, logger) {
   pattern = pattern.trim();
 
   if (false === isUnsafePattern(pattern)) {
@@ -22,11 +42,6 @@ module.exports = function main(directory, pattern, logger) {
   } else {
     logger.warn('Will not clean unsafe glob pattern; [[ %s ]] will find files outside of the target directory (%s)', pattern, directory);
   }
-};
-
-function isUnsafePattern(pattern) {
-  var startsWithDotDot = /^\.\./;
-  var startsWithSlash = /^\//;
-
-  return startsWithSlash.test(pattern) || startsWithDotDot.test(pattern);
 }
+
+module.exports = main;
